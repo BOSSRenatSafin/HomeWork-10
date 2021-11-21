5d6021d12cbd40acc6a9d07e6b2753e3600534be
Ренат <BOSS.Renat.Safin@gmail.com>
2021-11-21 10:49:02 +0300

    Initial Commit

---
+++ HomeWork 10/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  HomeWork 10
+//
+//  Created by Ренат Сафин on 21.11.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
